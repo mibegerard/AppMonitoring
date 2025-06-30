c2bd2b61d2454e2d06712815f1486fc97e475f8f
Mibe Keumeni Abba Gerard <mibekeumeni@icloud.com>
2025-06-30 16:38:35 +0200

    build workflow3

--- back/jest.config.js
+++ back/jest.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>/src/$1',
-    '^generated/(.*)$': '<rootDir>/generated/$1',
-  },
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    rootDir: '.',
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    testMatch: ['<rootDir>/src/**/*.spec.ts'],
+    moduleNameMapper: {
+        '^src/(.*)$': '<rootDir>/src/$1',
+        '^generated/(.*)$': '<rootDir>/generated/$1',
+    },
+    transform: {
+        '^.+\\.ts$': 'ts-jest',
+    },
 };
